7a382c0f5d999d0be29f8810c29fdf1655fe1bef
afgar88 <90805867+afgar88@users.noreply.github.com>
2022-03-16 21:51:42 -0400

    implemented presenters, and network utils

--- app/src/main/java/com/example/musicapp/rest/MusicApi.kt
+++ app/src/main/java/com/example/musicapp/rest/MusicApi.kt
@@ -23,13 +23,16 @@ interface MusicApi {
     fun getClassicalSongs(): Single<SongItem>
 
 
-    companion object{
-        const val BASE_URL="https://itunes.apple.com/"
-        private const val ROCK_SONGS ="search?term=rock&amp;media=music&amp;entity=song&amp;limit=50"
+    companion object {
+        const val BASE_URL = "https://itunes.apple.com/"
+        private const val ROCK_SONGS =
+            "search?term=rock&amp;media=music&amp;entity=song&amp;limit=50"
 
-        private const val POP_SONGS="search?term=pop&amp;media=music&amp;entity=song&amp;limit=50"
+        private const val POP_SONGS = "search?term=pop&amp;media=music&amp;entity=song&amp;limit=50"
 
-        private const val CLASSICAL_SONGS="search?term=classick&amp;media=music&amp;entity=song&amp;limit=50"
+        private const val CLASSICAL_SONGS =
+            "search?term=classick&amp;media=music&amp;entity=song&amp;limit=50"
+
 
 
     }
